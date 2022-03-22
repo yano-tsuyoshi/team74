0846bf56d842cb1aca20bf82a9439ea9ac2b0607
yano-tsuyoshi <positive.ninarou@icloud.com>
2022-03-22 19:11:39 +0900

    home画面作成

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BootstrapCSS  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <!-- CSS -->
+    <link rel="stylesheet" href="/css/teams.css">
+
+    <title>チーム開発 ホーム画面</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="side">
+            <div class="side-inner">
+                <ul class="nav flex-column">
+                    <p>なんとか鍼灸院</p>
+                    <li> <a href="">案内</a></li>
+                    <li><a href="">施術コース</a></li>
+                    <li>  <a href="">料金</a></li>
+                    <li>  <a href="">予約</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="main">
+            <div class="main-header">
+                <button type="button">新規登録</button>
+                <button type="button">ログイン</button>
+            </div>
+        </div>    
+    </div>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+ 
+</html>
