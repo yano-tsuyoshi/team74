7388090ccf1c27e205fc2d1adea58fd4cd4bb906
KoheyNishita <5024kohy.nishi@gmail.com>
2022-03-31 16:21:10 +0900

    予約画面のCSS

--- resources/views/reserves/reserve_page.blade.php
+++ resources/views/reserves/reserve_page.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.app')
-
 @section('content')
+@include('common.errors')
 
 <head>
     <!-- CSS -->
     <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1/themes/flick/jquery-ui.css">
-    <link rel="stylesheet" href="../layouts/reserve_page.css">
     <!-- JavaScript / jQuery -->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js"></script>
@@ -27,55 +26,59 @@
 <!-- 3. 時間あるなら予約データ編集・削除機能 -->
 
 <body>
-    <h1>予約フォーム</h1><br>
-    <form method="POST" action="{{ url('reserve') }}">
-        {{ csrf_field() }}
-        <p>
-            ユーザーID：<?php echo $user->id; ?><br>
-            予約者名：<?php echo $user->name; ?><br>
-        </p>
+    <div class="container">
+        <div class="wrapper">
+            <h1 class="topTitle">予約フォーム</h1><br>
+            <form method="POST" action="{{ url('reserve') }}">
+                {{ csrf_field() }}
+                <p>
+                    ユーザーID：<?php echo $user->id; ?><br>
+                    予約者名：<?php echo $user->name; ?><br>
+                </p>
 
-        <input type="hidden" value="<?php echo $user->id; ?>" name="user_id">
-        <input type="hidden" value="<?php echo $user->name; ?>" name="name">
+                <input type="hidden" value="<?php echo $user->id; ?>" name="user_id">
+                <input type="hidden" value="<?php echo $user->name; ?>" name="name">
 
-        カレンダーから日付を選択してください<br>
-        <input type="text" id="date_val" name="date"><br>
-        <div id="datepicker"></div><br>
+                カレンダーから日付を選択してください<br>
+                <input type="text" id="date_val" name="date"><br>
+                <div id="datepicker" class="datePicker"></div><br>
 
-        時間帯を選択してください<br>
-        <select name="time">
-            <option value="">選択してください</option>
-            <option value="10:00:00">10:00</option>
-            <option value="11:00:00">11:00</option>
-            <option value="12:00:00">12:00</option>
-            <option value="13:00:00">13:00</option>
-            <option value="14:00:00">14:00</option>
-            <option value="15:00:00">15:00</option>
-            <option value="16:00:00">16:00</option>
-            <option value="17:00:00">17:00</option>
-            <option value="18:00:00">18:00</option>
-        </select><br><br>
+                時間帯を選択してください<br>
+                <select name="time">
+                    <option value="">選択してください</option>
+                    <option value="10:00:00">10:00</option>
+                    <option value="11:00:00">11:00</option>
+                    <option value="12:00:00">12:00</option>
+                    <option value="13:00:00">13:00</option>
+                    <option value="14:00:00">14:00</option>
+                    <option value="15:00:00">15:00</option>
+                    <option value="16:00:00">16:00</option>
+                    <option value="17:00:00">17:00</option>
+                    <option value="18:00:00">18:00</option>
+                </select><br><br>
 
-        コースを選択してください<br>
-        <select name="course_id">
-            <option value="">選択してください</option>
-            @foreach($courses as $course)
-            <option value="{{ $course->id }}">{{ $course->name }}</option>
-            @endforeach
-        </select><br><br>
+                コースを選択してください<br>
+                <select name="course_id">
+                    <option value="">選択してください</option>
+                    @foreach($courses as $course)
+                    <option value="{{ $course->id }}">{{ $course->name }}</option>
+                    @endforeach
+                </select><br><br>
 
-        ご希望の施術スタッフを選択してください<br>
-        <select name="staff_id">
-            <option value="">選択してください</option>
-            @foreach($staffs as $staff)
-            <option value="{{ $staff->id }}">{{ $staff->name }}</option>
-            @endforeach
+                ご希望の施術スタッフを選択してください<br>
+                <select name="staff_id">
+                    <option value="">選択してください</option>
+                    @foreach($staffs as $staff)
+                    <option value="{{ $staff->id }}">{{ $staff->name }}</option>
+                    @endforeach
 
-        </select>
-        <br><br>
-        その他ご要望があればご記入ください<br>
-        <input type="text" value="" name="detail">
-        <br><br>
-        <input type="submit" value="予約する" class="submit">
-    </form>
+                </select>
+                <br><br>
+                その他ご要望があればご記入ください<br>
+                <input type="text" value="" name="detail">
+                <br><br>
+                <input type="submit" value="予約する" class="submit">
+            </form>
+        </div>
+    </div>
 </body>
