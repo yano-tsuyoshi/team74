f0637e8efb0e4193808fd143e6e49c8669403f6f
KoheyNishita <5024kohy.nishi@gmail.com>
2022-03-31 14:40:32 +0900

    予約システムのCSS／エラー表示

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <!-- CSS -->
     <link rel="stylesheet" href="/css/teams.css">
-
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
     <title>チーム開発 ホーム画面</title>
 </head>
 
--- resources/views/reserves/reserve_page.blade.php
+++ resources/views/reserves/reserve_page.blade.php
@@ -3,75 +3,79 @@
 @section('content')
 
 <head>
+    <!-- CSS -->
+    <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1/themes/flick/jquery-ui.css">
+    <link rel="stylesheet" href="../layouts/reserve_page.css">
+    <!-- JavaScript / jQuery -->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js"></script>
-    <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1/themes/flick/jquery-ui.css">
+    <script>
+        $(function() {
+            var dateFormat = 'y-m-d';
+            $("#datepicker").datepicker({
+                dateFormat: dateFormat,
+                minDate: 0,
+                onSelect: function(dateText, inst) {
+                    $("#date_val").val(dateText);
+                }
+            });
+        });
+    </script>
 </head>
 
 <!-- 2. フォームの見た目 -->
 <!-- 3. 時間あるなら予約データ編集・削除機能 -->
 
-<b>予約フォーム</b><br>
-<form method="POST" action="{{ url('reserve') }}">
-    {{ csrf_field() }}
-    <p>
-        ユーザーID：<?php echo $user->id; ?><br>
-        予約者名：<?php echo $user->name; ?>
+<body>
+    <h1>予約フォーム</h1><br>
+    <form method="POST" action="{{ url('reserve') }}">
+        {{ csrf_field() }}
+        <p>
+            ユーザーID：<?php echo $user->id; ?><br>
+            予約者名：<?php echo $user->name; ?><br>
+        </p>
 
         <input type="hidden" value="<?php echo $user->id; ?>" name="user_id">
         <input type="hidden" value="<?php echo $user->name; ?>" name="name">
-    </p><br>
 
-    1.カレンダーから日付を選択してください<br>
-    <input type="text" id="date_val" name="date"><br>
-    <div id="datepicker"></div><br>
+        カレンダーから日付を選択してください<br>
+        <input type="text" id="date_val" name="date"><br>
+        <div id="datepicker"></div><br>
 
-    2.時間帯を選択してください<br>
-    <select name="time">
-        <option value="">選択してください</option>
-        <option value="10:00:00">10:00</option>
-        <option value="11:00:00">11:00</option>
-        <option value="12:00:00">12:00</option>
-        <option value="13:00:00">13:00</option>
-        <option value="14:00:00">14:00</option>
-        <option value="15:00:00">15:00</option>
-        <option value="16:00:00">16:00</option>
-        <option value="17:00:00">17:00</option>
-        <option value="18:00:00">18:00</option>
-    </select><br><br>
+        時間帯を選択してください<br>
+        <select name="time">
+            <option value="">選択してください</option>
+            <option value="10:00:00">10:00</option>
+            <option value="11:00:00">11:00</option>
+            <option value="12:00:00">12:00</option>
+            <option value="13:00:00">13:00</option>
+            <option value="14:00:00">14:00</option>
+            <option value="15:00:00">15:00</option>
+            <option value="16:00:00">16:00</option>
+            <option value="17:00:00">17:00</option>
+            <option value="18:00:00">18:00</option>
+        </select><br><br>
 
-    3.コースを選択してください<br>
-    <select name="course_id">
-        <option value="">選択してください</option>
-        @foreach($courses as $course)
-        <option value="{{ $course->id }}">{{ $course->name }}</option>
-        @endforeach
-    </select><br><br>
+        コースを選択してください<br>
+        <select name="course_id">
+            <option value="">選択してください</option>
+            @foreach($courses as $course)
+            <option value="{{ $course->id }}">{{ $course->name }}</option>
+            @endforeach
+        </select><br><br>
 
-    5.ご希望の施術スタッフを選択してください<br>
-    <select name="staff_id">
-        <option value="">選択してください</option>
-        @foreach($staffs as $staff)
-        <option value="{{ $staff->id }}">{{ $staff->name }}</option>
-        @endforeach
+        ご希望の施術スタッフを選択してください<br>
+        <select name="staff_id">
+            <option value="">選択してください</option>
+            @foreach($staffs as $staff)
+            <option value="{{ $staff->id }}">{{ $staff->name }}</option>
+            @endforeach
 
-    </select>
-    <br><br>
-    6.その他ご要望があればご記入ください<br>
-    <input type="text" value="" name="detail">
-    <br><br>
-    <input type="submit" value="予約する" class="submit">
-</form>
-
-<script>
-    $(function() {
-        var dateFormat = 'y-m-d';
-        $("#datepicker").datepicker({
-            dateFormat: dateFormat,
-            minDate: 0,
-            onSelect: function(dateText, inst) {
-                $("#date_val").val(dateText);
-            }
-        });
-    });
-</script>
+        </select>
+        <br><br>
+        その他ご要望があればご記入ください<br>
+        <input type="text" value="" name="detail">
+        <br><br>
+        <input type="submit" value="予約する" class="submit">
+    </form>
+</body>
