42a3480ff45a5b78a5c5179820da675c5985611e
KoheyNishita <5024kohy.nishi@gmail.com>
2022-03-31 21:27:06 +0900

    予約画面のCSS

--- resources/views/reserves/reserve_page.blade.php
+++ resources/views/reserves/reserve_page.blade.php
@@ -28,56 +28,65 @@
 <body>
     <div class="container">
         <div class="wrapper">
-            <h1 class="topTitle">予約フォーム</h1><br>
+            <h1 class="topTitle">予約フォーム</h1>
             <form method="POST" action="{{ url('reserve') }}">
                 {{ csrf_field() }}
                 <p>
                     ユーザーID：<?php echo $user->id; ?><br>
-                    予約者名：<?php echo $user->name; ?><br>
+                    予約者名：<?php echo $user->name; ?>
                 </p>
-
                 <input type="hidden" value="<?php echo $user->id; ?>" name="user_id">
                 <input type="hidden" value="<?php echo $user->name; ?>" name="name">
 
-                カレンダーから日付を選択してください<br>
-                <input type="text" id="date_val" name="date"><br>
-                <div id="datepicker" class="datePicker"></div><br>
+                カレンダーから日付を選択してください
+                <div class="inputContents">
+                    <input type="text" id="date_val" name="date">
+                    <div id="datepicker" class="datePicker"></div>
+                </div>
 
-                時間帯を選択してください<br>
-                <select name="time">
-                    <option value="">選択してください</option>
-                    <option value="10:00:00">10:00</option>
-                    <option value="11:00:00">11:00</option>
-                    <option value="12:00:00">12:00</option>
-                    <option value="13:00:00">13:00</option>
-                    <option value="14:00:00">14:00</option>
-                    <option value="15:00:00">15:00</option>
-                    <option value="16:00:00">16:00</option>
-                    <option value="17:00:00">17:00</option>
-                    <option value="18:00:00">18:00</option>
-                </select><br><br>
+                <div class="inputContents">
+                    時間帯を選択してください
+                    <select name="time">
+                        <option value="">選択してください</option>
+                        <option value="10:00:00">10:00</option>
+                        <option value="11:00:00">11:00</option>
+                        <option value="12:00:00">12:00</option>
+                        <option value="13:00:00">13:00</option>
+                        <option value="14:00:00">14:00</option>
+                        <option value="15:00:00">15:00</option>
+                        <option value="16:00:00">16:00</option>
+                        <option value="17:00:00">17:00</option>
+                        <option value="18:00:00">18:00</option>
+                    </select>
+                </div>
 
-                コースを選択してください<br>
-                <select name="course_id">
-                    <option value="">選択してください</option>
-                    @foreach($courses as $course)
-                    <option value="{{ $course->id }}">{{ $course->name }}</option>
-                    @endforeach
-                </select><br><br>
+                <div class="inputContents">
+                    コースを選択してください
+                    <select name="course_id">
+                        <option value="">選択してください</option>
+                        @foreach($courses as $course)
+                        <option value="{{ $course->id }}">{{ $course->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
 
-                ご希望の施術スタッフを選択してください<br>
-                <select name="staff_id">
-                    <option value="">選択してください</option>
-                    @foreach($staffs as $staff)
-                    <option value="{{ $staff->id }}">{{ $staff->name }}</option>
-                    @endforeach
+                <div class="inputContents">
+                    ご希望の施術スタッフを選択してください
+                    <select name="staff_id">
+                        <option value="">選択してください</option>
+                        @foreach($staffs as $staff)
+                        <option value="{{ $staff->id }}">{{ $staff->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
 
-                </select>
-                <br><br>
-                その他ご要望があればご記入ください<br>
-                <input type="text" value="" name="detail">
-                <br><br>
-                <input type="submit" value="予約する" class="submit">
+                <div class="inputContents">
+                    その他ご要望があればご記入ください
+                    <input type="text" value="" name="detail" class="textAreaLarge">
+                </div>
+                <div class="inputContents">
+                    <input type="submit" value="予約する" class="btn-primary">
+                </div>
             </form>
         </div>
     </div>
