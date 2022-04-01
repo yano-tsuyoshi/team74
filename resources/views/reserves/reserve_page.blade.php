a0d921456516b77f1583f3260fab3a72ac8eec91
KoheyNishita <5024kohy.nishi@gmail.com>
2022-04-01 16:56:43 +0900

    予約画面作成：レイアウト

--- resources/views/reserves/reserve_page.blade.php
+++ resources/views/reserves/reserve_page.blade.php
@@ -38,14 +38,14 @@
                 <input type="hidden" value="<?php echo $user->id; ?>" name="user_id">
                 <input type="hidden" value="<?php echo $user->name; ?>" name="name">
 
-                カレンダーから日付を選択してください
+                <p class="formIndex">カレンダーから日付を選択してください</p>
                 <div class="inputContents">
                     <input type="text" id="date_val" name="date">
                     <div id="datepicker" class="datePicker"></div>
                 </div>
 
                 <div class="inputContents">
-                    時間帯を選択してください
+                    <p class="formIndex">時間帯を選択してください</p>
                     <select name="time">
                         <option value="">選択してください</option>
                         <option value="10:00:00">10:00</option>
@@ -61,7 +61,7 @@
                 </div>
 
                 <div class="inputContents">
-                    コースを選択してください
+                    <p class="formIndex">コースを選択してください</p>
                     <select name="course_id">
                         <option value="">選択してください</option>
                         @foreach($courses as $course)
@@ -71,7 +71,7 @@
                 </div>
 
                 <div class="inputContents">
-                    ご希望の施術スタッフを選択してください
+                    <p class="formIndex">ご希望の施術スタッフを選択してください</p>
                     <select name="staff_id">
                         <option value="">選択してください</option>
                         @foreach($staffs as $staff)
@@ -81,11 +81,11 @@
                 </div>
 
                 <div class="inputContents">
-                    その他ご要望があればご記入ください
+                    <p class="formIndex">その他ご要望があればご記入ください</p>
                     <textarea value="" name="detail" class="textAreaLarge"></textarea>
                 </div>
                 <div class="inputContents">
-                    <input type="submit" value="予約する" class="btn-primary">
+                    <button type="submit" class="btn-primary">予約する</button>
                 </div>
             </form>
         </div>
