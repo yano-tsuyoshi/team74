d03c395f407eb0a27aee98cc4ae4cc3550d2fc2c
KoheyNishita <5024kohy.nishi@gmail.com>
2022-04-07 16:00:13 +0900

    予約画面モーダルJS

--- public/js/reserve_modal_confirm.js
+++ public/js/reserve_modal_confirm.js
@@ -10,19 +10,17 @@ $(function () {
         $(this).on('click', function () {
             var target = $(this).data('target');
             var modal = document.getElementById(target);
-            $('body').css('overflow-y', 'hidden');
             $(modal).fadeIn();
 
             var title = $('#name').val()
             var modal = $(this)
             modal.find('#name').text(name)
-
+            
             return false;
         });
     });
     $('.js-modal-close').on('click', function () {
         $('.js-modal').fadeOut();
-        $('body').css('overflow-y', 'auto');
         return false;
     });
 });
