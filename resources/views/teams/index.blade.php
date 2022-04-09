686ee3404c9d4f76ca8ae659f5e33104248dc581
yano-tsuyoshi <positive.ninarou@icloud.com>
2022-04-09 17:11:40 +0900

    home画面、デザイン編集中

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -2,23 +2,154 @@
 
 @section('content')
 
-    <div class="container">
-        <div class="side">
-            <div class="side-inner">
-                <ul class="nav flex-column">
-                    <p>なんとか鍼灸院</p>
-                    <li> <a href="guide">案内</a></li>
-                    <li><a href="course">施術コース</a></li>
-                    <li>  <a href="">料金</a></li>
-                    <li>  <a href="reserve">予約</a></li>
-                </ul>
-            </div>
+    
+        <div class="container">
+            <h1>ようこそなんとか鍼灸院へ</h1>
+            <img src="/images/harikyuu.png">
+        </div>     
+
+    
+
+    <!-- <div class="container">
+      <main>
+        <div id="video">
+          <video class="bg-video" src="video/video.mp4" loop autoplay muted playsinline></video>
         </div>
-        <div class="main">
-            <div class="main-header">
-                
+
+        <section id="project">
+          <div class="wrapper">
+            <h2 class="sec-title">建築事例をご紹介</h2>
+            <ul class="list">
+              <li>
+                <a href="#">
+                  <img src="img/project1.jpg" alt="">
+                  <p class="name">PROJECT_01</p>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="img/project2.jpg" alt="">
+                  <p class="name">PROJECT_02</p>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="img/project3.jpg" alt="">
+                  <p class="name">PROJECT_03</p>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="img/project4.jpg" alt="">
+                  <p class="name">PROJECT_04</p>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </section>
+
+        <section id="feature">
+          <div class="wrapper">
+            <h2 class="sec-title">明るいHOUSEの特徴</h2>
+            <ul class="list">
+              <li>
+                <a href="#">design</a>
+                <p class="title">デザイン</p>
+                <p>テキストテキストテキスト</p>
+              </li>
+              <li>
+                <a href="#">quality</a>
+                <p class="title">性能</p>
+                <p>テキストテキストテキスト</p>
+              </li>
+              <li>
+                <a href="#">support</a>
+                <p class="title">サポート</p>
+                <p>テキストテキストテキスト</p>
+              </li>
+            </ul>
+          </div>
+        </section>
+
+        <section id="flow">
+          <div class="wrapper">
+            <h2 class="sec-title">家ができるまでの流れ</h2>
+            <p>お問い合わせいただいてから、家が完成するまでの流れです。</p>
+
+            <div class="step">
+              <ol class="figure">
+                <li>1.ヒアリング</li>
+                <li>2.プラン提案</li>
+                <li>3.契約</li>
+                <li>4.着工</li>
+                <li>5.完成</li>
+              </ol>
+
+              <dl class="description">
+                <dt><span>1</span>ヒアリング</dt>
+                <dd>
+                  テキストテキストテキストテキストテキストテキストテキストテキスト
+                  テキストテキストテキストテキストテキストテキストテキストテキスト
+                </dd>
+                <dt><span>2</span>プラン提案</dt>
+                <dd>
+                  テキストテキストテキストテキストテキストテキストテキストテキスト
+                  テキストテキストテキストテキストテキストテキストテキストテキスト
+                </dd>
+                <dt><span>3</span>契約</dt>
+                <dd>
+                  テキストテキストテキストテキストテキストテキストテキストテキスト
+                  テキストテキストテキストテキストテキストテキストテキストテキスト
+                </dd>
+                <dt><span>4</span>着工</dt>
+                <dd>
+                  テキストテキストテキストテキストテキストテキストテキストテキスト
+                  テキストテキストテキストテキストテキストテキストテキストテキスト
+                </dd>
+                <dt><span>5</span>完成</dt>
+                <dd>
+                  テキストテキストテキストテキストテキストテキストテキストテキスト
+                  テキストテキストテキストテキストテキストテキストテキストテキスト
+                </dd>
+              </dl>
             </div>
-        </div>    
+          </div>
+        </section>
+
+        <section id="contact">
+          <div class="wrapper">
+            <h2 class="sec-title">お問い合わせ</h2>
+            <p>お問い合わせは下記からお願いいたします。</p>
+            <div class="link">
+              <a class="item" href="#">
+                <img src="img/icon-document.png" alt=""><br>資料請求
+              </a>
+              <a class="item" href="#">
+                <img src="img/icon-seminar.png" alt=""><br>Web説明会
+              </a>
+              <a class="item" href="#">
+                <img src="img/icon-talk.png" alt=""><br>個別相談
+              </a>
+            </div>
+          </div>
+        </section>
+      </main>
+
+      <footer id="footer">
+        <a id="to-top" href="#"></a>
+
+        <ul class="menu">
+          <li><a href="#">私たちについて</a></li>
+          <li><a href="#">サービス</a></li>
+          <li><a href="#">商品情報</a></li>
+          <li><a href="#">展示会</a></li>
+          <li><a href="#">暮らしの日記</a></li>
+          <li><a href="#">会社概要</a></li>
+        </ul>
+        <p class="copyright">Copyright &copy; 2021 Akarui HOUSE All Rights Reserved.</p>
+      </footer>
     </div>
+  </body>
+</html> -->
 
 @endsection
